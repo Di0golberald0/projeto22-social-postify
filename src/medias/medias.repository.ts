5b7f7ce6fe7d35cf4d5c318403f8376e373feaae
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-08-29 20:15:46 -0300

    feat: Medias FindOne Implementado

--- src/medias/medias.repository.ts
+++ src/medias/medias.repository.ts
@@ -15,11 +15,13 @@ export class MediasRepository {
   }
 
   findAll() {
-    return `This action returns all medias`;
+    return this.prisma.media.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} media`;
+    return this.prisma.media.findUnique({
+      where: { id }
+    });
   }
 
   update(id: number, updateMediaDto: UpdateMediaDto) {
