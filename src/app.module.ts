c3cf0669419a2c1267eeccf2c0c1c305a906a5b1
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-08-29 20:14:45 -0300

    feat: Medias Create Implementado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MediasModule } from './medias/medias.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MediasModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/medias/medias.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMediaDto } from './dto/create-media.dto';
+import { UpdateMediaDto } from './dto/update-media.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MediasRepository {
+  
+  constructor(private readonly prisma: PrismaService) { }
+
+  create(createMediaDto: CreateMediaDto) {
+    return this.prisma.media.create ({
+      data: createMediaDto
+    })
+  }
+
+  findAll() {
+    return `This action returns all medias`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} media`;
+  }
+
+  update(id: number, updateMediaDto: UpdateMediaDto) {
+    return `This action updates a #${id} media`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} media`;
+  }
+}
