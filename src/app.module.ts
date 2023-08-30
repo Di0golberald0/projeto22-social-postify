33e57fe62faec65a4e7cadc083ba4ab4d898c7f3
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-08-29 21:04:50 -0300

    fix: App Module Atualizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MediasModule } from './medias/medias.module';
+import { PostsModule } from './posts/posts.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [PrismaModule, MediasModule],
+  imports: [PrismaModule, MediasModule, PostsModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { };
+
+export class AppModule {}
